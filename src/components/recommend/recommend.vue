fd5b23a8e33bd4a7dcd2c5a8df8dfe7fd82e7c00
tubring25 <tubring25@gmail.com>
2018-10-07 10:59:54 +0800

    day02

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,9 +1,37 @@
 <template>
-    <div>推荐</div>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper"></div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {}
 </script>
 <style lang="stylus" scoped>
+@import '../../common/stylus/variable.styl'
 
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
 </style>
